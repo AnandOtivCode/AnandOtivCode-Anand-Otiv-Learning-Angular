edd6079b707a7d0f983a7a904b31cdac5a07bb10
“Anand <w0858857@myscc.ca>
2025-09-27 08:41:05 -0400

    Updated the components so that there is an array of content items and migrate app component features down.

--- src/app/vehicle-list-item/vehicle-list-item.ts
+++ src/app/vehicle-list-item/vehicle-list-item.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Vehicle} from '../models/vehicle';
 
 @Component({
   selector: 'app-vehicle-list-item',
   imports: [],
   templateUrl: './vehicle-list-item.html',
+  standalone: true,
   styleUrl: './vehicle-list-item.css'
 })
 export class VehicleListItem {
+
+  @Input() vehicle?: Vehicle;
 
 }
