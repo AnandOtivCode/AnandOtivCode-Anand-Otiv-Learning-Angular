f28e08651ff8f3238e797936a00b64a4cc949243
“Anand <w0858857@myscc.ca>
2025-11-10 23:25:38 -0500

    Added Lazy Loading in main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,12 +18,25 @@ import {InMemoryDataService} from './app/services/in-memory-data.service';
 
 
 
+// const routes: Routes = [
+//   {path:'', redirectTo: '/vehicles', pathMatch: 'full'}, //default route
+//   { path: 'vehicles', component: VehicleList }, //Eagerly Loaded
+//   { path: 'vehicles/:id', component: VehicleListItem },
+//    {path:'modify-vehicle', component: ModifyVehicle},
+//    {path: '**', component:PageNotFound}//Wildcard route for a 404 page
+// ];
 const routes: Routes = [
-  {path:'', redirectTo: '/vehicles', pathMatch: 'full'}, //default route
-  { path: 'vehicles', component: VehicleList },
-  { path: 'vehicles/:id', component: VehicleListItem },
-   {path:'modify-vehicle', component: ModifyVehicle},
-   {path: '**', component:PageNotFound}//Wildcard route for a 404 page
+  { path: '', redirectTo: '/vehicles', pathMatch: 'full' }, //Default route
+  { path: 'vehicles', component: VehicleList },     //eagerly loaded
+  { path: 'vehicles/:id',
+    loadComponent: () =>
+      import('./app/vehicle-list-item/vehicle-list-item').then(m => m.VehicleListItem) }, //Lazy Loaded
+  { path: 'modify-vehicle',
+    loadComponent: () =>
+      import('./app/modify-vehicle/modify-vehicle').then(m => m.ModifyVehicle) },
+  { path: '**',
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found').then(m => m.PageNotFound) },
 ];
 // bootstrapApplication(AppComponent, appConfig)
 //   .catch((err) => console.error(err));
@@ -36,8 +49,8 @@ bootstrapApplication(AppComponent, {
 
 
 
-// So VehicleList Routing Works
-bootstrapApplication(VehicleList, {
-  providers: [provideRouter(routes)]
-}).then(r => console.log('Bootstrap successful'));
+// // So VehicleList Routing Works
+// bootstrapApplication(VehicleList, {
+//   providers: [provideRouter(routes)]
+// }).then(r => console.log('Bootstrap successful'));
 
