34b14e3f0976d65739608c22469b613a510571a1
“Anand <w0858857@myscc.ca>
2025-11-17 22:51:15 -0500

    Added the HighlightOnFocus Directive

---
+++ src/app/directives/highlight-on-focus.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+
+@Directive({
+  selector: '[appFocusHighlight]'
+})
+export class HighlightOnFocusDirective {
+  //Waiting to receive a colour from the parent component when called
+  @Input() appFocusHighlight = '';
+  //elementRef allows us to access the native DOM element assoiciated with the directive
+  constructor(private el: ElementRef) {}
+  /*
+  @HostListener('mouseenter'): This method is triggered when the mouse enters the element. It calls the highlight method, passing in
+  either the color specified in appHoverHighlight or a default color of yellow if none is provided.
+  @HostListener('mouseleave'): This method is triggered when the mouse leaves the element. It calls the highlight method with an
+  empty string, effectively removing the background color.
+   */
+  @HostListener('focus') onFocus() {
+    this.highlight(this.appFocusHighlight || 'yellow'); //Default colour just incase
+  }
+  @HostListener('blur') onBlur() {
+    this.highlight('');
+  }
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
+
